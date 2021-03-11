ae381248bbb02c3ac1940e01532aa6e62118c0b9
Adilson Angelo <adilson.angelo.jr@gmail.com>
2021-03-11 16:40:19 -0300

    splitting api/web

--- productsApp/app/models/product.rb
+++ productsApp/app/models/product.rb
@@ -1,12 +1,23 @@
 class Product
   include Mongoid::Document
   include Mongoid::Timestamps
-  validates :sku, :price, :name, :description, :amount, :presence => true
-  validates :sku, :uniqueness => true
+  validates_presence_of :sku, :price, :name, :description, :amount
+  validates_uniqueness_of :sku
 
   field :sku, type: String
   field :price, type: Float
   field :name, type: String
   field :description, type: String
   field :amount, type: Integer
+
+  def to_api
+    {
+      id: id.to_s,
+      sku: sku,
+      price: price,
+      name: name,
+      description: description,
+      amount: amount
+    }
+  end
 end
