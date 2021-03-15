7240c737abe1ff81f391cbcde447ffd6be577e1c
Adilson Angelo <adilson.angelo.jr@gmail.com>
2021-03-15 11:21:32 -0300

    Updates Product view

--- challenge2/phx_project/lib/phx_project_web/views/product_view.ex
+++ challenge2/phx_project/lib/phx_project_web/views/product_view.ex
@@ -3,11 +3,11 @@ defmodule PhxProjectWeb.ProductView do
   alias PhxProjectWeb.ProductView
 
   def render("index.json", %{products: products}) do
-    %{data: render_many(products, ProductView, "product.json")}
+    render_many(products, ProductView, "product.json")
   end
 
   def render("show.json", %{product: product}) do
-    %{data: render_one(product, ProductView, "product.json")}
+    render_one(product, ProductView, "product.json")
   end
 
   def render("product.json", %{product: product}) do
