f040cba4aed183d29d88b27d1a55423375bfe8cf
Adilson Angelo <adilson.angelo.jr@gmail.com>
2021-03-25 17:38:07 -0300

    update existing error_view tests

--- phoenix/phx_project/test/phx_project_web/views/error_view_test.exs
+++ phoenix/phx_project/test/phx_project_web/views/error_view_test.exs
@@ -5,11 +5,11 @@ defmodule PhxProjectWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.json" do
-    assert render(PhxProjectWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+    assert render(PhxProjectWeb.ErrorView, "404.json", []) == %{errors: %{message: "Not Found"}}
   end
 
   test "renders 500.json" do
     assert render(PhxProjectWeb.ErrorView, "500.json", []) ==
-             %{errors: %{detail: "Internal Server Error"}}
+             %{errors: %{message: "Internal Server Error"}}
   end
 end
