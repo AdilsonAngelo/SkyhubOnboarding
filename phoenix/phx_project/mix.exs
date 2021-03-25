2dfedf35ec8d1ffe54fdf36436e143a8d92152ed
Adilson Angelo <adilson.angelo.jr@gmail.com>
2021-03-25 16:53:47 -0300

    Fix tests settings

--- phoenix/phx_project/mix.exs
+++ phoenix/phx_project/mix.exs
@@ -60,7 +60,7 @@ defmodule PhxProject.MixProject do
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["test"]
     ]
   end
 end
