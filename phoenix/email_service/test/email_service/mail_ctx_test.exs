d11e90678afa0aecc0122ff04499478638f3e3f4
Adilson Angelo <adilson.angelo.jr@gmail.com>
2021-04-07 03:49:25 -0300

    email context and controller tests

--- phoenix/email_service/test/email_service/mail_ctx_test.exs
+++ phoenix/email_service/test/email_service/mail_ctx_test.exs
@@ -1,72 +1,72 @@
 defmodule EmailService.MailCtxTest do
-  use EmailService.DataCase
+  use ExUnit.Case
+  use Bamboo.Test
 
+  alias Bamboo.SentEmail
   alias EmailService.MailCtx
+  alias EmailService.MailCtx.Mailer
 
   describe "emails" do
     alias EmailService.MailCtx.Email
+    alias EmailService.MailCtx.Attachment
 
-    @valid_attrs %{attachments: "some attachments", body: "some body", from: "some from", subject: "some subject", to: "some to"}
-    @update_attrs %{attachments: "some updated attachments", body: "some updated body", from: "some updated from", subject: "some updated subject", to: "some updated to"}
-    @invalid_attrs %{attachments: nil, body: nil, from: nil, subject: nil, to: nil}
+    @valid_attrs %{
+      to: "test@test.com",
+      from: "test@test.com.br",
+      subject: "some subject",
+      body: "some body",
+      attachments: [%{filename: "test.csv", content: "testing,this,csv\n1,2,3"}]
+    }
+    @invalid_attrs %{
+      body: nil,
+      from: nil,
+      subject: nil,
+      to: nil
+    }
 
-    def email_fixture(attrs \\ %{}) do
-      {:ok, email} =
-        attrs
-        |> Enum.into(@valid_attrs)
+    test "validate_params/1 with valid data returns email" do
+      email = MailCtx.parse_params(@valid_attrs)
+      assert @valid_attrs.to == email.to
+      assert @valid_attrs.from == email.from
+      assert @valid_attrs.subject == email.subject
+      assert @valid_attrs.body == email.body
+      assert Enum.map(@valid_attrs.attachments, &struct!(%Attachment{}, &1)) == email.attachments
+    end
+
+    test "validate_params/1 with invalid data returns error" do
+      assert {:error, changeset} = MailCtx.parse_params(@invalid_attrs)
+      assert [
+        to: {"can't be blank", [validation: :required]},
+        from: {"can't be blank", [validation: :required]},
+        subject: {"can't be blank", [validation: :required]}
+      ] == changeset.errors
+    end
+
+    test "create_email/1 returns Bamboo.Email" do
+      email =
+        @valid_attrs
+        |> MailCtx.parse_params()
         |> MailCtx.create_email()
 
-      email
+      assert %Bamboo.Email{} = email
+
+      assert @valid_attrs.to == email.to
+      assert @valid_attrs.from == email.from
+      assert @valid_attrs.subject == email.subject
+      assert @valid_attrs.body == email.text_body
+      assert @valid_attrs.attachments
+        |> Enum.map(&struct!(%Attachment{}, &1))
+        |> Enum.map(&MailCtx.convert_attachment/1) == email.attachments
     end
 
-    test "list_emails/0 returns all emails" do
-      email = email_fixture()
-      assert MailCtx.list_emails() == [email]
-    end
+    test "Mailer.deliver_now/1 delivers the email" do
+      email =
+        @valid_attrs
+        |> MailCtx.parse_params()
+        |> MailCtx.create_email()
+        |> Mailer.deliver_now()
 
-    test "get_email!/1 returns the email with given id" do
-      email = email_fixture()
-      assert MailCtx.get_email!(email.id) == email
-    end
-
-    test "create_email/1 with valid data creates a email" do
-      assert {:ok, %Email{} = email} = MailCtx.create_email(@valid_attrs)
-      assert email.attachments == "some attachments"
-      assert email.body == "some body"
-      assert email.from == "some from"
-      assert email.subject == "some subject"
-      assert email.to == "some to"
-    end
-
-    test "create_email/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = MailCtx.create_email(@invalid_attrs)
-    end
-
-    test "update_email/2 with valid data updates the email" do
-      email = email_fixture()
-      assert {:ok, %Email{} = email} = MailCtx.update_email(email, @update_attrs)
-      assert email.attachments == "some updated attachments"
-      assert email.body == "some updated body"
-      assert email.from == "some updated from"
-      assert email.subject == "some updated subject"
-      assert email.to == "some updated to"
-    end
-
-    test "update_email/2 with invalid data returns error changeset" do
-      email = email_fixture()
-      assert {:error, %Ecto.Changeset{}} = MailCtx.update_email(email, @invalid_attrs)
-      assert email == MailCtx.get_email!(email.id)
-    end
-
-    test "delete_email/1 deletes the email" do
-      email = email_fixture()
-      assert {:ok, %Email{}} = MailCtx.delete_email(email)
-      assert_raise Ecto.NoResultsError, fn -> MailCtx.get_email!(email.id) end
-    end
-
-    test "change_email/1 returns a email changeset" do
-      email = email_fixture()
-      assert %Ecto.Changeset{} = MailCtx.change_email(email)
+      assert_delivered_email email
     end
   end
 end
--- phoenix/email_service/test/email_service_web/controllers/email_controller_test.exs
+++ phoenix/email_service/test/email_service_web/controllers/email_controller_test.exs
@@ -1,104 +1,48 @@
 defmodule EmailServiceWeb.EmailControllerTest do
   use EmailServiceWeb.ConnCase
+  use Bamboo.Test
 
   alias EmailService.MailCtx
   alias EmailService.MailCtx.Email
 
   @create_attrs %{
-    attachments: "some attachments",
-    body: "some body",
-    from: "some from",
+    to: "test@test.com",
+    from: "test@test.com.br",
     subject: "some subject",
-    to: "some to"
-  }
-  @update_attrs %{
-    attachments: "some updated attachments",
-    body: "some updated body",
-    from: "some updated from",
-    subject: "some updated subject",
-    to: "some updated to"
+    body: "some body",
+    attachments: [%{filename: "test.csv", content: "testing,this,csv\n1,2,3"}]
   }
   @invalid_attrs %{attachments: nil, body: nil, from: nil, subject: nil, to: nil}
 
-  def fixture(:email) do
-    {:ok, email} = MailCtx.create_email(@create_attrs)
-    email
+  describe "send_email" do
+    test "sends email with valid data", %{conn: conn} do
+      conn = post(conn, Routes.email_path(conn, :send_email), @create_attrs)
+
+      assert response(conn, 204)
+
+      assert_delivered_email create_email()
+    end
+
+    test "render errors and doesn't send email with invalid data", %{conn: conn} do
+      conn = post(conn, Routes.email_path(conn, :send_email), @invalid_attrs)
+      assert %{
+        "to" => ["can't be blank"],
+        "from" => ["can't be blank"],
+        "subject" => ["can't be blank"],
+        "attachments" => ["is invalid"],
+      } == json_response(conn, 422)["errors"]
+
+      assert_no_emails_delivered()
+    end
+  end
+
+  defp create_email() do
+    @create_attrs
+    |> MailCtx.parse_params()
+    |> MailCtx.create_email()
   end
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
-  end
-
-  describe "index" do
-    test "lists all emails", %{conn: conn} do
-      conn = get(conn, Routes.email_path(conn, :index))
-      assert json_response(conn, 200)["data"] == []
-    end
-  end
-
-  describe "create email" do
-    test "renders email when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.email_path(conn, :create), email: @create_attrs)
-      assert %{"id" => id} = json_response(conn, 201)["data"]
-
-      conn = get(conn, Routes.email_path(conn, :show, id))
-
-      assert %{
-               "id" => id,
-               "attachments" => "some attachments",
-               "body" => "some body",
-               "from" => "some from",
-               "subject" => "some subject",
-               "to" => "some to"
-             } = json_response(conn, 200)["data"]
-    end
-
-    test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.email_path(conn, :create), email: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
-    end
-  end
-
-  describe "update email" do
-    setup [:create_email]
-
-    test "renders email when data is valid", %{conn: conn, email: %Email{id: id} = email} do
-      conn = put(conn, Routes.email_path(conn, :update, email), email: @update_attrs)
-      assert %{"id" => ^id} = json_response(conn, 200)["data"]
-
-      conn = get(conn, Routes.email_path(conn, :show, id))
-
-      assert %{
-               "id" => id,
-               "attachments" => "some updated attachments",
-               "body" => "some updated body",
-               "from" => "some updated from",
-               "subject" => "some updated subject",
-               "to" => "some updated to"
-             } = json_response(conn, 200)["data"]
-    end
-
-    test "renders errors when data is invalid", %{conn: conn, email: email} do
-      conn = put(conn, Routes.email_path(conn, :update, email), email: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
-    end
-  end
-
-  describe "delete email" do
-    setup [:create_email]
-
-    test "deletes chosen email", %{conn: conn, email: email} do
-      conn = delete(conn, Routes.email_path(conn, :delete, email))
-      assert response(conn, 204)
-
-      assert_error_sent 404, fn ->
-        get(conn, Routes.email_path(conn, :show, email))
-      end
-    end
-  end
-
-  defp create_email(_) do
-    email = fixture(:email)
-    %{email: email}
   end
 end
