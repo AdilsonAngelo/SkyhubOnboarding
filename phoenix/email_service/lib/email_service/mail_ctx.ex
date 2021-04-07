6840d5254503d8514f5c1d760d87417c901e1646
Adilson Angelo <adilson.angelo.jr@gmail.com>
2021-04-07 03:47:37 -0300

    email controller & mail context updates

--- phoenix/email_service/lib/email_service/mail_ctx.ex
+++ phoenix/email_service/lib/email_service/mail_ctx.ex
@@ -2,17 +2,41 @@ defmodule EmailService.MailCtx do
   @moduledoc """
   The MailCtx context.
   """
+  import Bamboo.Email
 
   alias EmailService.MailCtx.Email
+  alias EmailService.MailCtx.Attachment
 
-  def validate_params(params \\ %{}) do
+  def parse_params(params \\ %{}) do
     changeset = Email.changeset(%Email{}, params)
 
     case changeset.valid? do
       :true ->
-        {:ok, Ecto.Changeset.apply_changes(changeset)}
+        Ecto.Changeset.apply_changes(changeset)
       :false ->
         {:error, changeset}
     end
+  end
+
+  def create_email(%Email{} = email) do
+    new =
+      new_email()
+      |> to(email.to)
+      |> from(email.from)
+      |> subject(email.subject)
+      |> text_body(email.body)
+
+    email.attachments
+    |> Enum.map(&convert_attachment/1)
+    |> Enum.reduce(new, fn x, acc ->
+      put_attachment(acc, x)
+    end)
+  end
+
+  def convert_attachment(%Attachment{} = attachment) do
+    %Bamboo.Attachment{
+      filename: attachment.filename,
+      data: attachment.content
+    }
   end
 end
--- phoenix/email_service/lib/email_service_web/controllers/email_controller.ex
+++ phoenix/email_service/lib/email_service_web/controllers/email_controller.ex
@@ -2,13 +2,18 @@ defmodule EmailServiceWeb.EmailController do
   use EmailServiceWeb, :controller
 
   alias EmailService.MailCtx
-  # alias EmailService.MailCtx.Email
+  alias EmailService.MailCtx.Email
+  alias EmailService.MailCtx.Mailer
 
   action_fallback EmailServiceWeb.FallbackController
 
   def send_email(conn, params) do
-    with {:ok, email} <- MailCtx.validate_params(params) do
-      render(conn, "show.json", email: email)
+    with %Email{} = email <- MailCtx.parse_params(params) do
+      email
+      |> MailCtx.create_email()
+      |> Mailer.deliver_now()
+
+      send_resp(conn, :no_content, "")
     end
   end
 end
