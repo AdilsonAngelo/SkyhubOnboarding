fff6ecc088b070bc4e099b7ed1676a8763a934f8
Adilson Angelo <adilson.angelo.jr@gmail.com>
2021-04-07 03:45:26 -0300

    update email and attachments schemas

--- phoenix/email_service/lib/email_service/mail_ctx/email.ex
+++ phoenix/email_service/lib/email_service/mail_ctx/email.ex
@@ -2,22 +2,27 @@ defmodule EmailService.MailCtx.Email do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @required_attrs [:to, :from, :subject]
-  @other_attrs [:body, :attachments]
-  @all_attrs @required_attrs ++ @other_attrs
+  alias EmailService.MailCtx.Attachment
 
+  @required_attrs [:to, :from, :subject]
+  @other_attrs [:body]
+  @all_fields @required_attrs ++ @other_attrs
+
+  @primary_key false
   schema "emails" do
     field :to, :string
     field :from, :string
     field :subject, :string
     field :body, :string
-    field :attachments, :string
+    embeds_many :attachments, Attachment
   end
 
   @doc false
   def changeset(email, attrs) do
     email
-    |> cast(attrs, @all_attrs)
+    |> cast(attrs, @all_fields)
+    |> cast_embed(:attachments, with: &Attachment.changeset/2)
     |> validate_required(@required_attrs)
   end
+
 end
--- phoenix/email_service/lib/email_service/mail_ctx/mailer.ex
+++ phoenix/email_service/lib/email_service/mail_ctx/mailer.ex
@@ -1,3 +1,3 @@
 defmodule EmailService.MailCtx.Mailer do
-  use Bamboo.Mailer, otp_app: :my_app
+  use Bamboo.Mailer, otp_app: :email_service
 end
